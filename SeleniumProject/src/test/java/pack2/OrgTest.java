411dcaf5989cce23ff1f84661e0459bffb20739a
anushaml <anushaml@DESKTOP-KTC0919>
2025-07-29 11:29:27 +0530

    selenium project is added

---
+++ SeleniumProject/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,24 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+@Test
+public void createOrgTest()
+{
+	String URL = System.getProperty("url");
+	String BROWSER = System.getProperty("browser", "chrome");
+	String USERNAME = System.getProperty("username");
+	String PASSWORD = System.getProperty("password");
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+}
+@Test
+public void modifyOrgTest()
+{
+	System.out.println("execute modifyOrgTest");
+}
+}
